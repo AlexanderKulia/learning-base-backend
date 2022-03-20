40c015eac7e1d2dc89ef9dad49f6729592fc8b0e
AlexanderKulia <37775912+AlexanderKulia@users.noreply.github.com>
2022-03-20 23:18:12 +0100

    testing ci/cd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "lb-b",
+      name: "lbb",
       script:
         "npm run db:migrate:prod && npm run db:generate && npm run build && npm run start:prod",
     },
